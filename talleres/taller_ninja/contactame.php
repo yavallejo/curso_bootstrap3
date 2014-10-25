e062b00edd2f46ce49f33c340bc843df2528eb14
Yan Arlex Vallejo <aprendiendoandoo@gmail.com>
2014-10-25 10:19:21 -0500

    clase 15

---
+++ talleres/taller_ninja/contactame.php
@@ -0,0 +1,96 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>ninjacode tv</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+		<div class="navbar">
+			<div class="navbar-inner">
+				<div class="container" style="width: auto;">
+					
+					<a class="brand" href="#">
+						<img src="img/ninja-code-tv.png" height="139" width="299" alt="">
+					</a>
+					<ul class="nav">
+							<li class="active"><a href="index.html">Home</a></li>
+							<li><a href="#">html5</a></li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li><a href="contactame.php">Contactame</a></li>
+						</ul>
+				</div>
+				<div class="redes_sociales">
+					<ul>
+						<li><a href="">
+							<img src="img/facebook_icon.png" height="30" width="30" alt="">
+						</a></li>
+						<li><a href="">
+							<img src="img/twitter_icon.png" height="30" width="30" alt="">
+						</a></li>
+						<li><a href="">
+							<img src="img/youtube_icon.png" height="30" width="30" alt="">
+						</a></li>
+					</ul>
+				</div>
+			</div><!-- /navbar-inner -->
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="span8">
+					<form class="form-horizontal">
+						<fieldset>
+							<legend>Contactame</legend>
+							<div class="control-group">
+								<label class="control-label" for="input01">Nombre</label>
+								<div class="controls">
+									<input type="text" class="input-xlarge" id="input01">
+									
+								</div>
+							</div>
+							<div class="control-group">
+								<label class="control-label" for="input01">correo</label>
+								<div class="controls">
+									<input type="text" class="input-xlarge" id="input01">
+									
+								</div>
+							</div>
+							<div class="control-group">
+								<label class="control-label" for="input01">Telefono</label>
+								<div class="controls">
+									<input type="text" class="input-xlarge" id="input01">
+									
+								</div>
+							</div>
+							
+							<div class="control-group">
+								<label class="control-label" for="textarea">Textarea</label>
+								<div class="controls">
+									<textarea class="input-xlarge" id="textarea" rows="3"></textarea>
+								</div>
+							</div>
+							<div class="form-actions">
+								<button type="submit" class="btn btn-danger">Enviar</button>
+								<button class="btn">Cancelar</button>
+							</div>
+						</fieldset>
+					</form>
+				</div>
+				<div class="span4">
+					<img src="img/codeninja.jpg" height="500" width="500" alt="">
+				</div>
+			</div>
+			
+		</div>
+		<footer>
+			ninja code tv
+		</footer>
+
+
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.js"></script>
+</body>
+</html>
